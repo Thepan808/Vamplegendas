54982eec27e3afb02dffa02462386ec128cb951f
Thepan808 <89053669+Thepan808@users.noreply.github.com>
2021-10-16 04:44:02 -0300

    Update help.py

--- ChannelBot/help.py
+++ ChannelBot/help.py
@@ -8,6 +8,6 @@ from pyrogram.types import InlineKeyboardMarkup
 async def _help(bot, msg):
     await bot.send_message(
         msg.chat.id,
-        "**Here's how to use me **\n" + Data.HELP,
+        "**♦️ Para me usar **\n" + Data.HELP,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons)
     )
