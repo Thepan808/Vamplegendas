68b4013051adb692e1904097743cd9ff1a6791d3
ঔৣ͜͡➳ 亇ቾ❍ ✟ア入れ刃入✟ <89053669+Thepan808@users.noreply.github.com>
2021-11-02 09:53:25 -0300

    Update report.py

--- ChannelBot/report.py
+++ ChannelBot/report.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@Client.on_message(filters.regex(r'^Report a Problem$') | filters.command('report'))
+@Client.on_message(filters.regex(r'^♦️Reportar o Problema♦️$') | filters.command('report'))
 async def _manage(_, msg):
     how = '**Reportar o Problema** \n\n'
     how += "Se caso algo acontecer, ou não sabe oque está fazendo **Doido da cabeça** bom, me comunique\n\n"
