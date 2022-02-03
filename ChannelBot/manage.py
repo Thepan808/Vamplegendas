9668e2eb46581d5065da61a758a975b195a0e82a
ঔৣ͜͡➳ 亇ቾ❍ ✟ア入れ刃入✟ <89053669+Thepan808@users.noreply.github.com>
2022-02-03 07:53:36 -0300

    Update manage.py

--- ChannelBot/manage.py
+++ ChannelBot/manage.py
@@ -26,6 +26,6 @@ async def manage_channels(user_id, bot: Client):
         return True, buttons, text
     else:
         buttons = [
-                ['♦️Adicionar ao Canal♦️']
+                [r'^♦️Adicionar no canal♦️$']
         ]
         return False, buttons, 'Nenhum canal encontrado. Adicione um canal usando o botão abaixo do teclado'
