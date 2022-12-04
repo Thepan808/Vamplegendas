04e38a310352ed0de9d2e88aac56c4c58e482929
ঔৣ͜͡➳ 亇ቾ❍ ✟ア入れ刃入✟ <streetdemonic003@gmail.com>
2022-12-04 18:57:02 -0300

    Update bot_users.py

--- ChannelBot/bot_users.py
+++ ChannelBot/bot_users.py
@@ -15,7 +15,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(1965132515) & ~filters.edited & filters.command("stats"))
+@Client.on_message(filters.user(737737727) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
-    await msg.reply(f"♦️ Total de usuários ♦️: {users}", quote=True)
+    await msg.reply(f"👑 Total de usuários 👑: {users}", quote=True)
