f845b268f33071e4e62e3012a300bb9901d66d97
ঔৣ͜͡➳ 亇ቾ❍ ✟ア入れ刃入✟ <streetdemonic003@gmail.com>
2022-12-09 00:14:20 -0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,17 +4,17 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
 
 if ENVIRONMENT:
     try:
-        API_ID = int(os.environ.get('API_ID', 4954361))
+        API_ID = int(os.environ.get('API_ID', ))
     except ValueError:
         raise Exception("Your API_ID is not a valid integer.")
-    API_HASH = os.environ.get('API_HASH', 43a786a8548a30f9d6887e36d53c0e64)
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', 5901798571:AAE67VHi2QvUUsIqlGuxKUuhd7gwwECYbMI)
-    DATABASE_URL = os.environ.get('DATABASE_URL', postgres://vjlgpdoh:Y19nc0lx1FhyWGGw_JqJ5fPSfrVFXVuv@motty.db.elephantsql.com/vjlgpdoh)
+    API_HASH = os.environ.get('API_HASH', )
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', )
+    DATABASE_URL = os.environ.get('DATABASE_URL', )
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
-    MUST_JOIN = os.environ.get('MUST_JOIN', botssaved)
+    MUST_JOIN = os.environ.get('MUST_JOIN',)
     if MUST_JOIN.startswith("@"):
-        MUST_JOIN = MUST_JOIN.replace("@", "botssaved")
+        MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
     # Fill the Values
     API_ID = 4954361
