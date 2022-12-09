6efbe3c26f62002d1395685847c764aad3aae5ca
ঔৣ͜͡➳ 亇ቾ❍ ✟ア入れ刃入✟ <streetdemonic003@gmail.com>
2022-12-09 02:11:30 -0300

    Update add_channels.py

--- ChannelBot/add_channels.py
+++ ChannelBot/add_channels.py
@@ -27,7 +27,7 @@ async def _add_channels(bot: Client, msg):
                             if chat_member_user.status in ['creator', 'administrator']:  # Don't allow non-admins.
                                 success, info = await get_channel_info(channel_id)
                                 if success:
-                                    else:
+                                    try:
                                         await uac(user_id, channel_id)
                                         await cac(channel_id, user_id)
                                         await channel.reply("Obrigado por me escolher. . . Lembre-se! Caso você tirar o bot em algum canal, remova o bot na opção Remover canal! Agora comece a gerenciar este canal personalizando as configurações abaixo.", quote=True)
