61431e6749ee3c6bb9103135c0afad85d5b2c223
The Vampiro Elegant <streetdemonic003@gmail.com>
2022-12-27 10:42:35 -0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -20,7 +20,7 @@ else:
     API_ID = 4954361
     API_HASH = "43a786a8548a30f9d6887e36d53c0e64"
     BOT_TOKEN = "5901798571:AAES32Up_Mbuop_iGaXMaNZqH0fySjhttZA"
-    DATABASE_URL = "postgres://toegfdjk:8FakmCkc4yNTdzbY8Xsf1k7U94bBsNST@motty.db.elephantsql.com/toegfdjk"
+    DATABASE_URL = "postgres://gpifiewb:yHors0isZWDhugTlnJOnKLNwpWSqaLoY@motty.db.elephantsql.com/gpifiewb"
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = "botssaved"
     if MUST_JOIN.startswith("@"):
